9a796eaf37155ac14e0c559e96f42b4a822ddc29
aialok <alokgupta1560@gmail.com>
2024-04-14 02:07:54 +0530

    Added Profile username, bio and edit profile button, country

--- client/src/components/Profile/MainProfile.jsx
+++ client/src/components/Profile/MainProfile.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineLocationOn , MdSchool} from "react-icons/md";
+// import profilePage from '../../assets/react.png'
 
 function MainProfile() {
   return (
-    <div>MainProfile</div>
-  )
+    <div className=" rounded-md min-h-[85vh] bg-[#282828]">
+      <div className="flex p-4 gap-4">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4jExqGXHj4gBQbuXB5O84RD8onDJ3GyQYMQ&s"
+          className="w-24 h-24 rounded-md"
+        />
+        <div className="profile-info">
+          <h1 className="text-xl font-bold mt-2">Alok Gupta</h1>
+          <h3 className="text-base text-[#9c9c99]">ai_alok</h3>
+          <p>Rank: {"10000"}</p>
+        </div>
+      </div>
+      <div className="p-4">
+        <p className="mb-2">I do code.....sometimes</p>
+        <button className="p-2 px-4 w-full rounded-md bg-[#283A2E] text-green-500">Edit Profile </button>
+        <div className="country mt-4 mb-2 flex items-center gap-2">
+          <MdOutlineLocationOn className="text-green-500 text-xl" />
+          <p>India</p>
+        </div>
+        <div className="studying flex items-center gap-2">
+          <MdSchool className="text-green-500 text-xl" />
+          <p>IIIT Ranchi</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MainProfile
+export default MainProfile;
--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import MainProfile from "../../components/Profile/MainProfile";
 function ProfilePage() {
   return (
-    <div className="mx-auto w-4/6 flex text-white mt-10">
-      <div className="w-[25%]">
+    <div className="mx-auto w-4/6 flex gap-4 text-white mt-10">
+      <div className="w-[30%]">
         <MainProfile />
       </div>
-      <div className="w-3/4">ProfileDashboard</div>
+      <div className="w-[70%] bg-[#282828] flex justify-center items-center rounded-md">Profile Dashboard</div>
     </div>
   );
 }
